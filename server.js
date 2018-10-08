c2d7bde661d1c87aec7ba416661d1b6f11ed6ed3
Camila Coder <camilanierocosta@hotmail.com>
2018-10-07 20:14:43 -0400

    user model created, working

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
 
 const users = require("./src/routes/apis/users");
 const profile = require("./src/routes/apis/profiles");
 const chats = require("./src/routes/apis/chats");
 
 const app = express();
+
+//Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //DB CONFIG
 const db = require("./config/mongoURI");
