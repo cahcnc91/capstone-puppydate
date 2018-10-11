0295ea5c7b29d653745623ad661d9ff72e83d75b
Camila Coder <camilanierocosta@hotmail.com>
2018-10-11 17:31:13 -0400

    react-redux authentication props token done

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
 import { Provider } from "react-redux";
 import store from "./store";
 import Navbar from "./components/layout/Navbar";
@@ -8,6 +11,27 @@ import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import "./App.css";
+
+//Check for token
+if (localStorage.jwtToken) {
+  //set head token header auth
+  setAuthToken(localStorage.jwtToken);
+  //decode token and get user info and exp
+  const decoded = jwt_decode(localStorage.jwtToken);
+  //Set user and isAuhtencticated
+  store.dispatch(setCurrentUser(decoded));
+
+  //Check for expired token
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    //Logout user
+    store.dispatch(logoutUser());
+    //TODO: clear current Profile
+
+    //Redirect to login
+    window.location.href = "/login";
+  }
+}
 
 class App extends Component {
   render() {
