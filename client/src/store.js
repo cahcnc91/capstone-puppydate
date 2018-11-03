ff775b9d646928cb93c1340d4d94c3a33fc377ee
Camila Coder <camilanierocosta@hotmail.com>
2018-11-03 11:32:43 -0400

    heroku test 4

--- client/src/store.js
+++ client/src/store.js
@@ -1,19 +1,18 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
-import { devToolsEnhancer } from "redux-devtools-extension/logOnlyInProduction";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 const initialState = {};
 
 const middleware = [thunk];
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+//const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   rootReducer,
   initialState,
-  composeEnhancers,
-  devToolsEnhancer(applyMiddleware(...middleware))
+  composeWithDevTools(applyMiddleware(...middleware))
 );
 
 export default store;
